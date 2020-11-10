a6af8fe39e843196799783ee9ce718c7de11b6a3
Avishkar Autar <aautar@digital-radiation.com>
2020-11-09 22:25:47 -0500

    minified dist build

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,30 @@
 import resolve from 'rollup-plugin-node-resolve';
+import minify from 'rollup-plugin-babel-minify';
 
-export default {
-    input: 'src/ForeignHtmlRenderer.mjs',
-    plugins: [
-        resolve({})
-    ],
-    output: {
-        name: 'ForeignHtmlRenderer',
-        file: 'dist/ForeignHtmlRenderer.js',
-        format: 'iife'
-    }
-};
+export default [
+    {
+        input: 'src/ForeignHtmlRenderer.mjs',
+        plugins: [
+            resolve({})
+        ],
+        output: {
+            name: 'ForeignHtmlRenderer',
+            file: 'dist/foreign-html-renderer.js',
+            format: 'iife'
+        }
+    },
+    {
+        input: 'src/ForeignHtmlRenderer.mjs',
+        plugins: [
+            resolve({}),
+            minify({
+                comments: false      
+            })        
+        ],
+        output: {
+            name: 'ForeignHtmlRenderer',
+            file: 'dist/foreign-html-renderer.min.js',
+            format: 'iife'
+        }
+    }    
+];
