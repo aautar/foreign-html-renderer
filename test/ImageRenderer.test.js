d7d75e4e952dc504b16838bc424a85a4dc9185ed
Avishkar Autar <aautar@digital-radiation.com>
2023-07-22 13:54:20 -0400

    cleanup test code, add comments

--- test/ImageRenderer.test.js
+++ test/ImageRenderer.test.js
@@ -3,6 +3,14 @@ const puppeteer = require("puppeteer");
 const express = require('express')
 
 test("renderToBase64Png generates correct base64 encoded PNG image", async () => {
+    /**
+     * 
+     * This is a very simply test to see if the red square (define by styled <div>) is rendered correctly
+     * Some caveats:
+     * -- The test has to be simple to account for rendering differences between browsers in different environments (e.g. between dev and CI environments)
+     * -- Check PNG output (in base64 or any respresentation) is probably not a good idea. It works here but, even with visually identical images, PNG may encode them differently or the encoder may add additional data
+     * 
+     */
     const expectedBase64PngOutput = await fs.readFile('test/assets/render-to-base64-png/expected-data-url-output.txt', { encoding: 'utf8' });
 
     const browser = await puppeteer.launch({"headless": "new"});
@@ -12,7 +20,7 @@ test("renderToBase64Png generates correct base64 encoded PNG image", async () =>
     ex.use(express.static('dist'));
 
     const port = 3736;
-    const testServer = ex.listen(port, () => { });
+    const testServer = ex.listen(port);
 
     try {
         const page = await browser.newPage();
